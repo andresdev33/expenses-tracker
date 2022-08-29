10f50844b6f91ff035abcfd54faa9aa8d50763e5
Andrés <cael19@gmail.com>
2022-08-28 23:14:53 -0500

    splitting components into multiple components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
     const expenses = [
         { title: 'Car Insurance', amount: 294.68, date: new Date(2022, 2, 28) },
-        { title: 'Car Insurance', amount: 284.68, date: new Date(2022, 2, 28) },
-        { title: 'Car Insurance', amount: 274.68, date: new Date(2022, 2, 28) },
-        { title: 'Car Insurance', amount: 264.68, date: new Date(2022, 2, 28) },
+        { title: 'Car Insurance', amount: 284.68, date: new Date(2022, 4, 14) },
+        { title: 'Car Insurance', amount: 274.68, date: new Date(2022, 6, 8) },
+        { title: 'Car Insurance', amount: 264.68, date: new Date(2022, 8, 18) },
     ];
 
     return (
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     const { date, title, amount } = props;
+
     return (
         <div className='expense-item'>
-            <div>{date.toISOString()}</div>
+            <ExpenseDate date={date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
