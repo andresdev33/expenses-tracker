8fa4f87153cdd562f30e9dbe1c6dc80c9275cef1
Andrés <cael19@gmail.com>
2022-08-29 21:12:36 -0500

    added component composition and props.children

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import './Card.css';
+
+export default function Card(props) {
+    const { children, className } = props;
+    const classes = 'card ' + className;
+    return <div className={classes}>{children}</div>;
+}
