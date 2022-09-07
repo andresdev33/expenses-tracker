a8d8a27b77618264cbcebab3a72e3f44d6238a16
Andrés <cael19@gmail.com>
2022-09-06 20:54:50 -0500

    add filter by year logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,10 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = expenses.filter(
+        (expense) => expense.date.getFullYear().toString() === filteredYear
+    );
+
     return (
         <div>
             <Card className='expenses'>
@@ -19,7 +23,7 @@ const Expenses = (props) => {
                     selectedYear={filteredYear}
                     onChangeFilter={changeFilterHandler}
                 />
-                {expenses.map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
